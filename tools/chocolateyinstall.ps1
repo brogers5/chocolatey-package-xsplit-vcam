65c0ba7448ec8e9eae7590cbbdf7d79e2e049679
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-06-27 18:07:04 -0400

    Update to v4.0.2206.2307

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ Confirm-Win10
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
-$softwareVersion = '4.0.2205.3103'
+$softwareVersion = '4.0.2206.2307'
 
 $installedVersion = Get-CurrentVersion
 if ([Version] $softwareVersion -lt [Version] $installedVersion)
@@ -18,8 +18,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'XSplit VCam'
   fileType       = 'MSI'
-  url64bit       = 'https://cdn2.xsplit.com/download/vc/4.0.2205.3103/XSplit_VCam_4.0.2205.3103.msi'
-  checksum64     = '33c4f13a7142520cad2d7dc01e69ccefff7082699a35e719dbb8391d90931287'
+  url64bit       = 'https://cdn2.xsplit.com/download/vc/4.0.2206.2307/XSplit_VCam_4.0.2206.2307.msi'
+  checksum64     = '6be9baf98edd5e3535bcc1482afb2664fbfaf0823e1eabc45e478f8a83dc884e'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
