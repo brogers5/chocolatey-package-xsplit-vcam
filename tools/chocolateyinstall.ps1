c6f4930455cda69e60e027495060a887a1789fd1
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-05-03 15:29:22 -0400

    Update to 4.1.2304.2701

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ Confirm-Win10
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
-[version] $softwareVersion = '4.1.2303.2101'
+[version] $softwareVersion = '4.1.2304.2701'
 $installedVersion = Get-CurrentVersion
 if ($softwareVersion -lt $installedVersion) {
   Write-Output "Current installed version (v$installedVersion) must be uninstalled first..."
@@ -16,8 +16,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'XSplit VCam'
   fileType       = 'MSI'
-  url64bit       = 'https://cdn2.xsplit.com/download/vc/4.1.2303.2101/XSplit_VCam_4.1.2303.2101.msi'
-  checksum64     = 'bc6ba66b2cd396a7232c6bd8afc5c672611669a97a1abff8f66b47ff3e48d773'
+  url64bit       = 'https://cdn2.xsplit.com/download/vc/4.1.2304.2701/XSplit_VCam_4.1.2304.2701.msi'
+  checksum64     = 'a1f6bb00c8df7ccd3a5a296f5a297efdb1ce37d25d1a6f2d719339d04e7e0269'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
