bc94efeacee7f49331d13e790c7fec635c59688d
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-07-19 10:30:25 -0400

    Skip installation if packaged software version is already installed

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,20 +7,27 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 [version] $softwareVersion = '4.1.2306.2801'
 $installedVersion = Get-CurrentVersion
-if ($softwareVersion -lt $installedVersion) {
-  Write-Output "Current installed version (v$installedVersion) must be uninstalled first..."
-  Uninstall-CurrentVersion
-}
 
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  softwareName   = 'XSplit VCam'
-  fileType       = 'MSI'
-  url64bit       = 'https://cdn2.xsplit.com/download/vc/4.1.2306.2801/XSplit_VCam_4.1.2306.2801.msi'
-  checksum64     = '7c4771e5e810d154b894071722c2b703a0aa021607d6fd2746f0369310c021d0'
-  checksumType64 = 'sha256'
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes = @(0, 3010, 1641)
+if ($installedVersion -and $installedVersion -eq $softwareVersion -and !$env:ChocolateyForce) {
+  Write-Output "$softwareName v$installedVersion is already installed."
+  Write-Output 'Skipping download and execution of installer.'
 }
+else {
+  if ($softwareVersion -lt $installedVersion) {
+    Write-Output "Current installed version (v$installedVersion) must be uninstalled first..."
+    Uninstall-CurrentVersion
+  }
 
-Install-ChocolateyPackage @packageArgs
+  $packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    softwareName   = 'XSplit VCam'
+    fileType       = 'MSI'
+    url64bit       = 'https://cdn2.xsplit.com/download/vc/4.1.2306.2801/XSplit_VCam_4.1.2306.2801.msi'
+    checksum64     = '7c4771e5e810d154b894071722c2b703a0aa021607d6fd2746f0369310c021d0'
+    checksumType64 = 'sha256'
+    silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+    validExitCodes = @(0, 3010, 1641)
+  }
+
+  Install-ChocolateyPackage @packageArgs
+}
